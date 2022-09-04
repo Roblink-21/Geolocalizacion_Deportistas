3c4768ba3a7fbbc3b52749fa350e99524bf6481e
Carlos Quel <and.quel5@gmail.com>
2022-09-04 11:35:30 -0500

    Adding project

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+const config: SocketIoConfig = {url: 'http://localhost:3000', options: {}}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SocketIoModule.forRoot(config),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'migps-230df',
+    appId: '1:427961845234:web:adf24803542460105b576b',
+    databaseURL: 'https://migps-230df-default-rtdb.firebaseio.com',
+    storageBucket: 'migps-230df.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA3AAm4sqhs1ofdFrEjBmBL-MTOdDG63Nk',
+    authDomain: 'migps-230df.firebaseapp.com',
+    messagingSenderId: '427961845234',
+  },
+  production: true
+};
